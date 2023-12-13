50badd1953f35a926f84b2a90d1165de7405b4b5
Adam <78027161+NoSpawnn@users.noreply.github.com>
2023-12-13 10:31:56 +0000

    so... close....

--- 2023/c++/day2_solution.cpp
+++ 2023/c++/day2_solution.cpp
@@ -3,7 +3,8 @@
 2. Get game ID
 3. Split line into sets by semicolon
 4. Split sets into individual draws
-5. Count up for each draw and check valid, this game is valid if all draws are valid
+5. Count up for each draw and check valid, this game is valid if all draws are
+valid
 */
 
 #include <algorithm>
@@ -20,6 +21,7 @@
 #define GREEN "green"
 
 int get_game_id_and_cut_str(std::string *s);
+std::vector<std::string> split_into_sets(std::string line);
 
 std::map<std::string, int> MAX_COUNTS = {{RED, 12}, {BLUE, 14}, {GREEN, 13}};
 
@@ -35,26 +37,32 @@ int main() {
   while (std::getline(input, line)) {
     int game_id = get_game_id_and_cut_str(&line);
 
-    std::istringstream iss(line);
-    std::string word;
+    bool valid = true;
+    std::string color;
     int count;
-    while (iss >> count) {
-      bool valid = true;
-      std::map<std::string, int> counts = {{RED, 0}, {BLUE, 0}, {GREEN, 0}};
-      iss >> word;
+    std::vector<std::string> sets = split_into_sets(line);
 
-      word.erase(std::remove(word.begin(), word.end(), ','), word.end());
-      word.erase(std::remove(word.begin(), word.end(), ';'), word.end());
+    for (auto set : sets) {
+      std::cout << set << "\n";
+
+      std::istringstream iss(set);
+      std::map<std::string, int> counts = {{RED, 0}, {BLUE, 0}, {GREEN, 0}};
+
+      while (iss >> count) {
+        iss >> color;
+
+        counts[color] += count;
+      }
 
       for (auto [color, count] : counts) {
         if (count > MAX_COUNTS[color]) {
           valid = false;
         }
       }
+    }
 
-      if (valid) {
-        total += game_id;
-      }
+    if (valid) {
+      total += game_id;
     }
   }
 
@@ -70,4 +78,17 @@ int get_game_id_and_cut_str(std::string *s) {
   *s = s->substr(colon_pos + 2, s->length() - colon_pos - 1);
 
   return id;
+}
+
+std::vector<std::string> split_into_sets(std::string line) {
+  std::vector<std::string> draws;
+
+  size_t pos = 0;
+  while (pos != std::string::npos) {
+    pos = line.find(";");
+    draws.push_back(line.substr(0, pos));
+    line = line.substr(pos + 2, line.length() - pos - 1);
+  }
+
+  return draws;
 }
