f94a5e2e5006b1c86afb7f712e5cf6984010ba6f
= <=>
2023-12-20 16:26:19 +0000

    smarter implementation using builtin functions

--- 2023/c++/day1.cpp
+++ 2023/c++/day1.cpp
@@ -5,82 +5,47 @@
 #include <iostream>
 #include <map>
 #include <string>
-#include <vector>
 
-void replace_words(std::string *line);
-std::array<int, 2> find_numbers(std::string s);
+using namespace std;
 
-const std::map<std::string, std::string> WORDS_TO_NUMS = {
-    {"one", "o1e"},   {"two", "t2o"},   {"three", "t3e"},
-    {"four", "f4r"},  {"five", "f5e"},  {"six", "s6x"},
-    {"seven", "s7n"}, {"eight", "e8t"}, {"nine", "n9e"}};
+int p1_total = 0;
+int p2_total = 0;
+
+void replace_words(string *line) {
+  static const map<string, string> WORDS_TO_NUMS = {
+      {"one", "o1e"},   {"two", "t2o"},   {"three", "t3e"},
+      {"four", "f4r"},  {"five", "f5e"},  {"six", "s6x"},
+      {"seven", "s7n"}, {"eight", "e8t"}, {"nine", "n9e"}};
+
+  size_t match_start = 0;
+  for (auto [match, replace] : WORDS_TO_NUMS) {
+    while ((match_start = line->find(match)) != string::npos) {
+      line->replace(match_start, replace.length(), replace);
+      match_start += replace.length();
+    }
+  }
+}
 
 int main() {
-  std::ifstream input("../input/day1.txt");
+  ifstream input("../input/day1.txt");
 
   if (!input.is_open()) {
     perror("ERROR - failed to open file:");
   }
 
-  std::string line;
-  std::vector<std::array<int, 2>> numbers1;
-  std::vector<std::array<int, 2>> numbers2;
+  string line;
   while (input >> line) {
-    numbers1.push_back(find_numbers(line));
+    p1_total += (line[line.find_first_of("123456789")] - 0x30) * 10 +
+                (line[line.find_last_of("123456789")] - 0x30);
 
     replace_words(&line);
-    numbers2.push_back(find_numbers(line));
+    p2_total += (line[line.find_first_of("123456789")] - 0x30) * 10 +
+                (line[line.find_last_of("123456789")] - 0x30);
   }
 
-  int p1_total = 0;
-  for (auto v : numbers1) {
-    p1_total += v[0] * 10 + v[1];
-  }
+  cout << "P1 TOTAL: " << p1_total << endl;
+  cout << "P2 TOTAL: " << p2_total << endl;
 
-  int p2_total = 0;
-  for (auto v : numbers2) {
-    p2_total += v[0] * 10 + v[1];
-  }
-
-  std::cout << "P1 TOTAL: " << p1_total << std::endl;
-  std::cout << "P2 TOTAL: " << p2_total << std::endl;
-
+  input.close();
   return 0;
-}
-
-std::array<int, 2> find_numbers(std::string s) {
-  std::array<int, 2> numbers;
-
-  // Find first number
-  for (size_t i = 0; i < s.length(); i++) {
-    if (!std::isdigit(s[i])) {
-      continue;
-    }
-
-    numbers[0] = s[i] - '0';
-    break;
-  }
-
-  // Find last number
-  for (size_t i = s.length() - 1; i >= 0; i--) {
-    if (!std::isdigit(s[i])) {
-      continue;
-    }
-
-    numbers[1] = s[i] - '0';
-    break;
-  }
-
-  return numbers;
-}
-
-void replace_words(std::string *line) {
-  size_t start = 0;
-
-  for (auto [word, replace] : WORDS_TO_NUMS) {
-    while ((start = line->find(word)) != std::string::npos) {
-      line->replace(start, replace.length(), replace);
-      start += replace.length();
-    }
-  }
 }
