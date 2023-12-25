0507e872d879942ea85b4ad1ded4ef362652f1de
NoSpawnn <78027161+NoSpawnn@users.noreply.github.com>
2023-12-25 11:17:36 +0000

    standardized!

--- 2023/c++/day_09.cpp
+++ 2023/c++/day_09.cpp
@@ -1,9 +1,11 @@
 // https://adventofcode.com/2023/day/9
 
+#include "common.hpp"
 #include <algorithm>
 #include <fstream>
 #include <iostream>
 #include <iterator>
+#include <numeric>
 #include <sstream>
 #include <vector>
 
@@ -32,41 +34,44 @@ static vector<int> find_diffs(const vector<int> &nums) {
 static int find_next(const vector<int> &nums) {
   vector<int> diffs = find_diffs(nums);
 
-  if (zeroed(diffs)) {
-    return nums.back() + diffs.back();
-  }
-
-  return nums.back() + find_next(diffs);
+  return zeroed(diffs) ? nums.back() + diffs.back()
+                       : nums.back() + find_next(diffs);
 }
 
 static int find_prev(const vector<int> &nums) {
   vector<int> diffs = find_diffs(nums);
 
-  if (zeroed(diffs)) {
-    return nums.front() - diffs.front();
-  }
+  return zeroed(diffs) ? nums.front() - diffs.front()
+                       : nums.front() - find_prev(diffs);
+}
 
-  return nums.front() - find_prev(diffs);
+static int part_1(ifstream &input) {
+  vector<string> lines = get_lines(input);
+
+  return std::accumulate(lines.begin(), lines.end(), 0, [](int acc, string s) {
+    istringstream iss(s);
+    vector<int> nums(istream_iterator<int>(iss), {});
+    return acc += find_next(nums);
+  });
+}
+
+static int part_2(ifstream &input) {
+  vector<string> lines = get_lines(input);
+
+  return std::accumulate(lines.begin(), lines.end(), 0, [](int acc, string s) {
+    istringstream iss(s);
+    vector<int> nums(istream_iterator<int>(iss), {});
+    return acc += find_prev(nums);
+  });
 }
 
 int main() {
   ifstream input("in.txt");
 
-  int total1 = 0;
-  int total2 = 0;
-  string line;
-  while (getline(input, line)) {
-    istringstream iss(line);
-    vector<int> nums(istream_iterator<int>(iss), {});
-
-    total1 += find_next(nums);
-    total2 += find_prev(nums);
-
-    nums.clear();
-  }
-
-  cout << total1 << endl;
-  cout << total2 << endl;
+  cout << part_1(input) << endl;
+  input.clear();
+  input.seekg(ifstream::beg);
+  cout << part_2(input) << endl;
 
   return 0;
 }
