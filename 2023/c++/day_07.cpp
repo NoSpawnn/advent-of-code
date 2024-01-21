0caddbdec60a787b49df947e87049c0c0bfad90a
NoSpawnn <78027161+NoSpawnn@users.noreply.github.com>
2024-01-21 17:33:44 +0000

    rewrite and part 2 done

--- 2023/c++/day_07.cpp
+++ 2023/c++/day_07.cpp
@@ -1,144 +1,155 @@
 // https://adventofcode.com/2023/day/7
 
 #include <algorithm>
-#include <cstddef>
-#include <cstdlib>
 #include <fstream>
 #include <iostream>
 #include <map>
-#include <set>
 #include <string>
 #include <vector>
 
-using namespace std;
+using std::cerr;
+using std::cout;
+using std::endl;
+using std::ifstream;
+using std::map;
+using std::string;
+using std::vector;
 
-#define FIVE_KIND "five"
-#define FOUR_KIND "four"
-#define FULL_HOUSE "full"
-#define THREE_KIND "three"
-#define TWO_PAIR "two"
-#define ONE_PAIR "one"
-#define HIGH_CARD "high"
+static int categorise_hand(const string cards, const string strengths,
+                           bool jokers);
+
+const char JOKER = 'J';
+
+enum HAND_TYPE {
+  HIGH_CARD = 1,
+  ONE_PAIR,
+  TWO_PAIR,
+  THREE_KIND,
+  FULL_HOUSE,
+  FOUR_KIND,
+  FIVE_KIND
+};
 
 typedef struct Hand {
   int bet;
-  string type;
+  int type;
   string cards;
 } Hand;
 
-static int total1, total2;
-static vector<Hand> hands;
-static const map<char, int> CARD_STRENGTHS_1 = {
-    {'2', 1}, {'3', 2}, {'4', 3},  {'5', 4},  {'6', 5},  {'7', 6},  {'8', 7},
-    {'9', 8}, {'T', 9}, {'J', 10}, {'Q', 11}, {'K', 12}, {'A', 13},
-};
-static const map<char, int> CARD_STRENGTHS_2 = {
-    {'J', 1}, {'2', 2}, {'3', 3},  {'4', 4},  {'5', 5},  {'6', 6},  {'7', 7},
-    {'8', 8}, {'9', 9}, {'T', 10}, {'Q', 11}, {'K', 12}, {'A', 13},
-};
-static const map<string, int> HAND_STRENGTHS = {
-    {HIGH_CARD, 1},  {ONE_PAIR, 2},  {TWO_PAIR, 3},  {THREE_KIND, 4},
-    {FULL_HOUSE, 5}, {FOUR_KIND, 6}, {FIVE_KIND, 7},
-};
+static map<char, int> count_cards(string cards) {
+  map<char, int> cards_count;
 
-struct {
-  bool operator()(Hand a, Hand b) const {
-    if (HAND_STRENGTHS.at(a.type) < HAND_STRENGTHS.at(b.type))
-      return true;
-    if (HAND_STRENGTHS.at(a.type) > HAND_STRENGTHS.at(b.type))
-      return false;
+  for (const char &c : cards) {
+    cards_count[c]++;
+  }
 
-    for (size_t i = 0; i < 5; ++i) {
-      if (CARD_STRENGTHS_1.at(a.cards[i]) < CARD_STRENGTHS_1.at(b.cards[i]))
-        return true;
-      if (CARD_STRENGTHS_1.at(a.cards[i]) > CARD_STRENGTHS_1.at(b.cards[i]))
-        return false;
+  return cards_count;
+}
+
+int best_alternate(string cards, const string &strengths,
+                   const int jokers_count) {
+  switch (categorise_hand(cards, strengths, false)) {
+  case FIVE_KIND:
+    return FIVE_KIND;
+  case FOUR_KIND:
+    return FIVE_KIND;
+  case FULL_HOUSE:
+    switch (jokers_count) {
+    case 1:
+      return FOUR_KIND;
+    case 2:
+      return FIVE_KIND;
+    case 3:
+      return FIVE_KIND;
+    }
+  case THREE_KIND:
+    return FOUR_KIND;
+  case TWO_PAIR:
+    switch (jokers_count) {
+    case 1:
+      return FULL_HOUSE;
+    case 2:
+      return FOUR_KIND;
+    }
+  case ONE_PAIR:
+    return THREE_KIND;
+  case HIGH_CARD:
+    return ONE_PAIR;
+  default:
+    cerr << "bad cards? " << cards << endl;
+    exit(EXIT_FAILURE);
+  }
+}
+
+static int categorise_hand(const string cards, const string card_strengths,
+                           bool jokers) {
+  map<char, int> cards_count = count_cards(cards);
+
+  if (jokers && cards_count.count(JOKER)) {
+    return best_alternate(cards, card_strengths, cards_count.at(JOKER));
+  }
+
+  switch (cards_count.size()) {
+  case 5:
+    return HIGH_CARD;
+  case 4:
+    return ONE_PAIR;
+  case 3:
+    return std::any_of(cards_count.begin(), cards_count.end(),
+                       [](auto count) { return count.second == 2; })
+               ? TWO_PAIR
+               : THREE_KIND;
+  case 2:
+    return std::abs(cards_count.begin()->second -
+                    cards_count.rbegin()->second) == 3
+               ? FOUR_KIND
+               : FULL_HOUSE;
+  case 1:
+    return FIVE_KIND;
+  default:
+    cerr << "hands should be of length five, got length " << cards.length()
+         << " (" << cards << ")" << endl;
+    exit(EXIT_FAILURE);
+  }
+}
+
+static int solve(ifstream &input, const string card_strengths, bool jokers) {
+  int total = 0, bet;
+  string cards;
+  vector<Hand> hands;
+
+  while (input >> cards >> bet) {
+    hands.push_back(
+        Hand{bet, categorise_hand(cards, card_strengths, jokers), cards});
+  }
+
+  std::sort(hands.begin(), hands.end(), [card_strengths](Hand a, Hand b) {
+    if (a.type != b.type) {
+      return a.type < b.type;
+    }
+
+    for (size_t i = 0; i < a.cards.length(); i++) {
+      int score_a = card_strengths.find(a.cards[i]);
+      int score_b = card_strengths.find(b.cards[i]);
+
+      if (score_a != score_b) {
+        return score_a < score_b;
+      }
     }
 
     return true;
-  }
-} card_comp;
+  });
 
-string categorize_hand(string cards) {
-  map<char, int> card_occurences;
-
-  for (auto card : cards) {
-    card_occurences[card] += 1;
+  for (size_t rank = 0; rank < hands.size(); rank++) {
+    total += hands[rank].bet * (rank + 1);
   }
 
-  int unique_cards = card_occurences.size();
-
-  switch (unique_cards) {
-  case 1:
-    return FIVE_KIND;
-
-    break;
-  case 2: {
-    auto it = card_occurences.begin();
-    int card1_count = it->second;
-    ++it;
-    int card2_count = it->second;
-
-    if (abs(card1_count - card2_count) == 3) {
-      return FOUR_KIND;
-    } else {
-      return FULL_HOUSE;
-    }
-
-    break;
-  }
-  case 3: {
-    auto it = card_occurences.begin();
-    int card1_count = it->second;
-    ++it;
-    int card2_count = it->second;
-    ++it;
-    int card3_count = it->second;
-
-    if (card1_count == 2 || card2_count == 2 || card3_count == 2) {
-      return TWO_PAIR;
-    } else {
-      return THREE_KIND;
-    }
-
-    break;
-  }
-  case 4:
-    return ONE_PAIR;
-
-    break;
-  case 5:
-    return HIGH_CARD;
-
-    break;
-  default:
-    return NULL;
-  }
+  return total;
 }
 
-static int part_1(ifstream &input) {
-  int ans = 0;
-  string cards;
-  int bet;
+int part_1(ifstream &input) { return solve(input, "23456789TJQKA", false); }
 
-  while (input >> cards >> bet) {
-    hands.push_back(Hand{bet, categorize_hand(cards), cards});
-  }
-
-  sort(hands.begin(), hands.end(), card_comp);
-
-  for (int i = 0; i < hands.size(); ++i) {
-    ans += hands[i].bet * (i + 1);
-  }
-
-  return ans;
-}
-
-static int part_2(ifstream &input) {
-  int ans = 0;
-
-  return ans;
-}
+int part_2(ifstream &input) { return solve(input, "J23456789TQKA", true); }
 
 int main() {
   ifstream input("../input/day_07.txt");
