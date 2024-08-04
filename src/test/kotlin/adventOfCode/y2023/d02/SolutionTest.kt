bda7495291d1f2b4dcf20f031b1a0db12b5c237d
NoSpawnn <78027161+NoSpawnn@users.noreply.github.com>
2024-08-04 14:49:57 +0100

    2023 Day 2

---
+++ src/test/kotlin/adventOfCode/y2023/d02/SolutionTest.kt
@@ -0,0 +1,16 @@
+package adventOfCode.y2023.d02
+
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+class SolutionTest {
+
+    @Test
+    fun part1() {
+    }
+
+    @Test
+    fun part2() {
+    }
+}
