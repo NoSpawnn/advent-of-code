f313574782bb1c168947d195310992fbbebc2098
NoSpawnn <78027161+NoSpawnn@users.noreply.github.com>
2024-08-04 01:55:09 +0100

    Add tests

---
+++ src/test/kotlin/adventOfCode/y2015/d02/SolutionTest.kt
@@ -0,0 +1,28 @@
+package adventOfCode.y2015.d02
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class SolutionTest {
+    private val part1TestInputs = mapOf(
+        "2x3x4" to 58, "1x1x10" to 43
+    )
+
+    private val part2TestInputs = mapOf(
+        "2x3x4" to 34, "1x1x10" to 14
+    )
+
+    @Test
+    fun part1() {
+        for ((input, expected) in part1TestInputs) {
+            assertEquals(Solution().part1(input), expected)
+        }
+    }
+
+    @Test
+    fun part2() {
+        for ((input, expected) in part2TestInputs) {
+            assertEquals(Solution().part2(input), expected)
+        }
+    }
+}
---
+++ src/test/kotlin/adventOfCode/y2023/d01/SolutionTest.kt
@@ -0,0 +1,35 @@
+package adventOfCode.y2023.d01
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class SolutionTest {
+    private val part1TestInput = """
+        1abc2
+        pqr3stu8vwx
+        a1b2c3d4e5f
+        treb7uchet
+    """.trimIndent()
+    private val part1Expected = 142
+
+    private val part2TestInput = """
+        two1nine
+        eightwothree
+        abcone2threexyz
+        xtwone3four
+        4nineeightseven2
+        zoneight234
+        7pqrstsixteen
+    """.trimIndent()
+    private val part2Expected = 281
+
+    @Test
+    fun part1() {
+        assertEquals(Solution().part1(part1TestInput), part1Expected)
+    }
+
+    @Test
+    fun part2() {
+        assertEquals(Solution().part2(part2TestInput), part2Expected)
+    }
+}
